7b0a90c3ebf22d996bbc9f11d8c6cf9f76d35179
Abdden Giffren <giffrenabdden@gmail.com>
2022-11-29 17:05:53 +0200

    Committing all the changes at once

---
+++ base/js/popup.js
@@ -0,0 +1,11 @@
+let popUp = document.getElementById('container-article');
+
+let open = document.getElementById('open');
+
+function openPopUp() {
+  popUp.classList.add('poppedup');
+}
+
+function closePopUp() {
+  popUp.classList.remove('poppedup');
+}
